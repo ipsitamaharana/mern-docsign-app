83bf720064b92e143b0036a24c3fc64fdcc63292
Ipsita Maharana <ipsitamaharana70@gmail.com>
2026-06-18 13:09:47 +0530

    Day  8 completed - generated signed PDF using PDF-Lib

--- Backend/routes/signatureRoutes.js
+++ Backend/routes/signatureRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const fs = require("fs");
+const path = require("path");
+const { PDFDocument, rgb } = require("pdf-lib");
+
 const Signature = require("../models/Signature");
+const File = require("../models/File");
 
 router.post("/", async (req, res) => {
   try {
@@ -14,6 +19,72 @@ router.post("/", async (req, res) => {
     });
 
     res.status(201).json(signature);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+router.post("/generate/:fileId", async (req, res) => {
+  try {
+    const file = await File.findById(req.params.fileId);
+
+    if (!file) {
+      return res.status(404).json({
+        message: "File not found",
+      });
+    }
+
+    const signature = await Signature.findOne({
+      fileId: req.params.fileId,
+    });
+
+    if (!signature) {
+      return res.status(404).json({
+        message: "Signature not found",
+      });
+    }
+
+    const pdfBytes = fs.readFileSync(file.path);
+
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+
+    const pages = pdfDoc.getPages();
+    const firstPage = pages[0];
+
+    firstPage.drawText(`Signed By: ${signature.signer}`, {
+      x: signature.coordinates.x,
+      y: signature.coordinates.y,
+      size: 18,
+      color: rgb(0, 0, 0),
+    });
+
+    const signedPdfBytes = await pdfDoc.save();
+
+    const signedDir = "uploads/signed";
+
+    if (!fs.existsSync(signedDir)) {
+      fs.mkdirSync(signedDir, { recursive: true });
+    }
+
+    const signedFileName = `signed-${file.filename}`;
+
+    const signedPath = path.join(
+      signedDir,
+      signedFileName
+    );
+
+    fs.writeFileSync(
+      signedPath,
+      signedPdfBytes
+    );
+
+    res.status(200).json({
+      message: "Signed PDF generated successfully",
+      file: signedPath,
+    });
+
   } catch (error) {
     res.status(500).json({
       message: error.message,
